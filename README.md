e586c72c6fd7c8165831f301f3f1c833fd6f8bcb
Ramlo moha <134856218+Ramla9528@users.noreply.github.com>
2024-03-27 08:09:27 +0300

    last commits modified getComputerChoice function since we did not need user input for the computer  but insteed we needed the computer to chooose either rock,paper or scissors randomly  second modified the playround function to let the userput defferent forms of the choices third declared a function that loops over the playround five times, calling the it five times to play five round game. also declared another function that is gonna determine the winner according to the score. and explained little bit about the game in our README file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rockPS
-this my first js project where I will be creating a Rock Paper Scissors Game. 
+This is my first js project where I will be creating a Rock Paper Scissors Game. 
+In this project we created rock,paper, scissors game that will be player against the computer and will determine the winner 
--- rock.js
+++ rock.js
@@ -1,32 +1,66 @@
-'use in strict';
-//declaring a function either returns rock, paper or scissors randomly
-function getComputerChoice(){
-    //declare a variable to store an array that consists of (rock, paper, and scissors)
-    const choices = ['rock', 'paper', 'scissors'];
-    //return a random choice from math.random
-return choices[(Math.floor(Math.random() * choices.length))];
+
+//declare a function that lets the computer choose either rock,paper or scissors randomly.
+function getComputerChoice(){ 
+  choices = ['rock', 'paper', 'scissors'];
+  return choices[(Math.floor(Math.random() * choices.length))];
 }
-getComputerChoice();
+
 //declare function that playes one round of game
+//compare if the choices of the computer and the player are the same or not
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-    //stating the winner according to choice of the computer and the userplayer
-    if(playerSelection === computerSelection){
+  if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
         return 'its tie';
-    }else if(playerSelection === 'rock' && computerSelection === 'paper'){
-    return 'you lose, wraps rock';
-   }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
-    return 'you lose, scissors cut paper';
-   }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-    return 'congratulations you won, scissors cut paper';
-   }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
-    return 'congratulations, you won rock crashes scissors';
-   }else if(playerSelection === 'paper' && computerSelection === 'rock'){
-    return 'you won, paper wraps rock';
-   }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+    }else if((playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper')){
+    return 'you lost, paper wraps rock';
+   }else if((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors')){
+    return 'you won, rock crashes scissors';
+   }else if((playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock')){
     return 'you lost, rock crashes scissors';
+   }else if((playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')){
+    return ' you won, scissors cut paper';
+   }else if((playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors')){
+    return 'you lost, scissors cut paper';
+   }else if((playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')){
+    return 'you won, paper wraps rock';
    }
 }
-  const playerSelection = "scissors";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+//function that let the game to play five rounds
+//if the userplayer wins playerscore is incremented otherwise computerscore is incremented.
+playerScore = 0;
+computerScore = 0;
+  function playgame(){
+    for(let i = 0; i < 5; i++){
+    const playerSelection = prompt('make a choise');
+    const computerSelection = getComputerChoice();
+    console.log((computerSelection));
+      console.log(playRound(playerSelection,computerSelection));
+      console.log(`User ${playerScore} VS Computer ${computerScore}`)
+      if((playRound(playerSelection,computerSelection) === ("you won, scissors cuts paper"))||(playRound(playerSelection,computerSelection) === ("you won, rock crashes scissors."))||(playRound(playerSelection,computerSelection) === ("you won, paper wraps rock")))
+      {
+        playerScore++;
+      }
+       else if((playRound(playerSelection,computerSelection)  === ('you lost, paper wraps rock'))||(playRound(playerSelection,computerSelection) === ('you lost, rock crashes scissors'))||(playRound(playerSelection,computerSelection) === ('you lost, scissors cut paper'))){
+        computerScore++;
+       }
+    }
+    getTheWinner();
+}
+//this function checkes which score is greater or lesser and logs massege to console.
+//this function called in the playgame function after the five round game to determine the winnner. 
+ function getTheWinner(){
+  console.log('Game over');
+  if (playerScore > computerScore){
+    console.log('congratulation you win the game.')
+  }else if(playerScore < computerScore){
+    console.log('try next time')
+  }else {
+    console.log('its tie')
+  }
+ } 
+    
+ playgame();
+
+
+
+
+
