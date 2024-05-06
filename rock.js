bde504656e8c6dd77cdd09c9f322b2d8df479d15
Ramlo moha <134856218+Ramla9528@users.noreply.github.com>
2024-05-06 08:12:20 +0300

    made UI for the rock paper scissors game

--- rock.js
+++ rock.js
@@ -17,50 +17,48 @@ function playRound(playerSelection, computerSelection) {
    }else if((playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock')){
     return 'you lost, rock crashes scissors';
    }else if((playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper')){
-    return ' you won, scissors cut paper';
+    return 'you won, scissors cut paper';
    }else if((playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors')){
     return 'you lost, scissors cut paper';
    }else if((playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')){
     return 'you won, paper wraps rock';
    }
 }
-//function that let the game to play five rounds
-//if the userplayer wins playerscore is incremented otherwise computerscore is incremented.
 playerScore = 0;
 computerScore = 0;
-  function playgame(){
-    for(let i = 0; i < 5; i++){
-    const playerSelection = prompt('make a choise');
-    const computerSelection = getComputerChoice();
-    console.log((computerSelection));
-      console.log(playRound(playerSelection,computerSelection));
-      console.log(`User ${playerScore} VS Computer ${computerScore}`)
-      if((playRound(playerSelection,computerSelection) === ("you won, scissors cuts paper"))||(playRound(playerSelection,computerSelection) === ("you won, rock crashes scissors."))||(playRound(playerSelection,computerSelection) === ("you won, paper wraps rock")))
-      {
-        playerScore++;
-      }
-       else if((playRound(playerSelection,computerSelection)  === ('you lost, paper wraps rock'))||(playRound(playerSelection,computerSelection) === ('you lost, rock crashes scissors'))||(playRound(playerSelection,computerSelection) === ('you lost, scissors cut paper'))){
-        computerScore++;
-       }
-    }
-    getTheWinner();
-}
-//this function checkes which score is greater or lesser and logs massege to console.
-//this function called in the playgame function after the five round game to determine the winnner. 
- function getTheWinner(){
-  console.log('Game over');
-  if (playerScore > computerScore){
-    console.log('congratulation you win the game.')
-  }else if(playerScore < computerScore){
-    console.log('try next time')
-  }else {
-    console.log('its tie')
+
+
+const container = document.querySelector(".container");
+const round = document.querySelector("#round");
+ const options = document.querySelector("#options");
+
+ options.addEventListener("click", function(e){
+  if(playerScore < 5 && computerScore < 5){
+  let target = e.target;
+  const playerSelection = target.id;
+ const computerSelection = getComputerChoice(); 
+round.textContent = playRound(playerSelection, computerSelection);
+
+
+ if((playRound(playerSelection,computerSelection) === ("you won, scissors cut paper"))
+ ||(playRound(playerSelection,computerSelection) === ("you won, rock crashes scissors."))
+||(playRound(playerSelection,computerSelection) === ("you won, paper wraps rock"))){
+  playerScore++;
+ }
+  else if((playRound(playerSelection,computerSelection)  === ('you lost, paper wraps rock'))
+  ||(playRound(playerSelection,computerSelection) === ('you lost, rock crashes scissors'))
+||(playRound(playerSelection,computerSelection) === ('you lost, scissors cut paper'))){
+   computerScore++;
   }
- } 
-    
- playgame();
-
-
-
-
+  const score = document.querySelector("#display-score");
+  score.textContent = `User Score ${playerScore}  Vs  Computer Score ${computerScore} `;
+}
+  if(playerScore == 5){
+    const winner = document.querySelector("#winner");
+    winner.textContent = "congratualtions you win the Game!";
+  } else if (computerScore == 5){
+    const winner = document.querySelector("#winner");
+    winner.textContent = "computer wins the Game!";
+  }
+});
 
